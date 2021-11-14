f3b18b90def8490fc5d34a720be643cf11830617
MustafaRaafat <49460931+MustafaRaafat@users.noreply.github.com>
2021-11-14 13:41:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,18 +2,39 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express =  require('express');
 
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
+
 // Setup Server
+const port =8000;
+const Server=app.listen(port,listening);
+
+function listening() {
+    console.log(`server running on port : ${port}`);
+}
+const data=[];
+app.post("/mustafa", function(req,res) {
+data.push(req.body);
+console.log(data)
+})
+
+const citydata=[];
+app.post("/getcity",function(req,res) {
+    citydata.push(req.body);
+    console.log(citydata);
+})
