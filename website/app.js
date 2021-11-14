a5d775ce636a7ff60dd9ad09bc623802b83e0a6b
MustafaRaafat <49460931+MustafaRaafat@users.noreply.github.com>
2021-11-14 13:42:23 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,5 +1,29 @@
 /* Global Variables */
+const apiKey=',us&appid=ed2c201aaf3417d9ae88d06b67764e60'
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e){
+const newCity =  document.getElementById('zip').value;
+getCity(baseURL,newCity, apiKey)
+
+}
+const getCity = async (baseURL, city, key)=>{
+
+  const res = await fetch(baseURL+city+key)
+  try {
+
+    const data = await res.json();
+    console.log(data)
+    document.getElementById('date').innerHTML = data[0].animal;
+    document.getElementById('temp').innerHTML = data[0].facts;
+    document.getElementById('content').innerHTML = data[0].fav;
+    return data;
+  }  catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+}
+getCity('/getcity',);
