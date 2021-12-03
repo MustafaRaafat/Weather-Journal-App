00c15674a5dfa085993ace8d33af5de764e52ef1
MustafaRaafat <49460931+MustafaRaafat@users.noreply.github.com>
2021-12-03 16:15:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,44 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 
 // Require Express to run server and routes
-const express =  require('express');
+const express=require('express');
 
 // Start up an instance of app
-const app = express();
+const app=express();
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser')
+const bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors=require('cors');
 app.use(cors());
+
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
-
 // Setup Server
-const port =8000;
-const Server=app.listen(port,listening);
 
-function listening() {
-    console.log(`server running on port : ${port}`);
+const port=8000;
+
+const server=app.listen(port,lisme);
+
+function lisme() {
+    console.log(`server running on port: ${port}`)
 }
-const data=[];
-app.post("/mustafa", function(req,res) {
-data.push(req.body);
-console.log(data)
-})
+// Initialize all route with a callback function
 
-const citydata=[];
-app.post("/getcity",function(req,res) {
-    citydata.push(req.body);
-    console.log(citydata);
-})
+// Callback function to complete GET '/city'
+app.get('/city',function (req,res) {
+    res.send(projectData);
+    console.log('getme');
+});
+
+// Post Route
+app.post('/city',setCity);
+function setCity(req,res) {
+    projectData=req.body;
+}
